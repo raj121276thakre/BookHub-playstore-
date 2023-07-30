0791e9b7394aff29260f8c9a67f6528e07b26f71
raj121276thakre <raj121276thakre@gmail.com>
2023-07-30 15:45:11 +0530

    30/07 nav drawaer part_1

--- app/src/main/java/com/rajapps/bookify_test/MainActivity.kt
+++ app/src/main/java/com/rajapps/bookify_test/MainActivity.kt
@@ -3,7 +3,15 @@ package com.rajapps.bookify_test
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.MenuItem
+import android.widget.FrameLayout
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.navigation.NavigationView
 import com.rajapps.bookify_test.Adapters.HomeAdapter
 import com.rajapps.bookify_test.Models.HomeModel
 import com.rajapps.bookify_test.Repository.MainRepo
@@ -28,10 +36,82 @@ class MainActivity : AppCompatActivity() {
         ViewModelProvider(activity, MainViewModelFactory(repo))[MainViewModel::class.java]
     }
 
+    // navigation
+    lateinit var drawerLayout : DrawerLayout
+    lateinit var coordinatorLayout : CoordinatorLayout
+    lateinit var toolbar : Toolbar
+    lateinit var framelayout : FrameLayout
+    lateinit var navigationview : NavigationView
+    var previousMenuItem : MenuItem? = null
+    // navigation
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+// navigation
+        drawerLayout = findViewById(R.id.drawerLayout)
+        coordinatorLayout = findViewById(R.id.coordinatorLayout)
+        toolbar = findViewById(R.id.toolbar)
+        framelayout = findViewById(R.id.framelayout)
+        navigationview = findViewById(R.id.navigationview)
+
+        setUpToolbar()
+
+        val actionBarDrawerToggle = ActionBarDrawerToggle(this,drawerLayout,
+            R.string.open_drawer,
+            R.string.close_drawer
+        )
+
+        drawerLayout.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
+
+
+        navigationview.setNavigationItemSelectedListener {
+
+            if (previousMenuItem != null){
+                previousMenuItem?.isChecked =false
+            }
+
+            it.isCheckable = true
+            it.isChecked = true
+            previousMenuItem = it
+
+            when(it.itemId){
+
+                R.id.dashboard -> {
+
+
+                    drawerLayout.closeDrawers()
+                }
+
+                R.id.favourites ->{
+
+
+                    supportActionBar?.title="Favourites"
+                    drawerLayout.closeDrawers()
+                }
+
+                R.id.profile ->{
+
+                    supportActionBar?.title="Profile"
+                    drawerLayout.closeDrawers()
+                }
+
+                R.id.about ->{
+
+                    supportActionBar?.title="About App"
+                    drawerLayout.closeDrawers()
+                }
+
+            }
+
+            return@setNavigationItemSelectedListener true
+        }
+
+        // navigation
+
 
         binding.apply {
             mRvHome.adapter = adapter
@@ -47,7 +127,7 @@ class MainActivity : AppCompatActivity() {
 
        // supportActionBar?.title= ""
 
-    }
+    }// functions defined below
 
     private fun handleHomeBackend() {
         viewModel.homeLiveData.observe(activity) {
@@ -81,6 +161,28 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+
+    //navigation
+    fun setUpToolbar(){
+        setSupportActionBar(toolbar)
+        supportActionBar?.title="BookHub"
+        supportActionBar?.setHomeButtonEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        val id = item.itemId
+
+        if (id == android.R.id.home){
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    //navigation
 }
 
 
