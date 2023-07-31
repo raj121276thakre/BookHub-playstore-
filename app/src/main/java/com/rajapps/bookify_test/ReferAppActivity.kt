1da67853a975b6d05ab524e8cabb43fc98187ac5
raj121276thakre <raj121276thakre@gmail.com>
2023-08-01 00:09:31 +0530

    31/07 refer page Ui

---
+++ app/src/main/java/com/rajapps/bookify_test/ReferAppActivity.kt
@@ -0,0 +1,11 @@
+package com.rajapps.bookify_test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ReferAppActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_refer_app)
+    }
+}
